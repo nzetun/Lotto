cdb12158e47bda746d12f6b31921141626f37eb1
Noah Zetocha <63809979+nzetun@users.noreply.github.com>
2021-06-23 14:51:49 -0500

    updated documentation

--- Lotto/src/lotto/LottoRun.java
+++ Lotto/src/lotto/LottoRun.java
@@ -1,5 +1,9 @@
 package lotto;
 
+/**
+* @author nzetocha
+* Class that runs the lottery ticket and outputs to the standard output.
+*/
 public class LottoRun {
 	
 	public static void main(String args[]) {
